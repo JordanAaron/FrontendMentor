f53bfcbf3df4953b512fa1dbda10ce00e46555f0
Jordan Quartey <jordev14@gmail.com>
2023-12-06 19:18:18 +0000

    feat: add styles for mobile view

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -12,34 +12,35 @@ export default function NewsletterPage() {
 	return (
 		<div className={styles.container}>
 			<Image src={mobileIllustration} alt="sign up illustration" />
-			<h1 className={styles.header}>Stay updated!</h1>
-			<p className={styles.paragraph}>
-				Join 60,000+ product managers receiving monthly updates on:
-			</p>
-			<ul>
-				<li>
-					<Image src={listIcon} alt="list icon" />
-					<p>Product discovery and building what matters</p>
-				</li>
-				<li>
-					<Image src={listIcon} alt="list icon" />
-					<p>Measuring to ensure updates are a success</p>
-				</li>
-				<li>
-					<Image src={listIcon} alt="list icon" />
-					<p>And much more!</p>
-				</li>
-			</ul>
-			<form
-				onSubmit={(e) => {
-					e.preventDefault()
-					console.log(e.target)
-				}}
-			>
-				<label htmlFor="emailAddress">Email address</label>
-				<input type="email" id="emailAddress" placeholder="email@company.com" />
-				<button type="submit">Subscribe to monthly newsletter</button>
-			</form>
+			<div className={styles.content}>
+				<h1>Stay updated!</h1>
+				<p>Join 60,000+ product managers receiving monthly updates on:</p>
+				<ul>
+					<li>
+						<Image src={listIcon} alt="list icon" />
+						<p>Product discovery and building what matters</p>
+					</li>
+					<li>
+						<Image src={listIcon} alt="list icon" />
+						<p>Measuring to ensure updates are a success</p>
+					</li>
+					<li>
+						<Image src={listIcon} alt="list icon" />
+						<p>And much more!</p>
+					</li>
+				</ul>
+				<form>
+					<div className={styles.emailEntry}>
+						<label htmlFor="emailAddress">Email address</label>
+						<input
+							type="email"
+							id="emailAddress"
+							placeholder="email@company.com"
+						/>
+					</div>
+					<button type="submit">Subscribe to monthly newsletter</button>
+				</form>
+			</div>
 		</div>
 	)
 }
